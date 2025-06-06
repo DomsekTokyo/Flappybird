1ede17ae4dddb8d53ea100ee6b4ba2cdf259905c
Dominik <mikuleckydominik@gmail.com>
2025-06-06 17:46:17 +0200

    základní nastavení flappy bird

--- flappybird.py
+++ flappybird.py
@@ -2,4 +2,19 @@ from pygame import *
 init()
 
 
+
+width = 900
+height = 800
+okno = display.set_mode((width, height))
+
+fps = 60
+time = time.Clock()
+
+
+yes = True
+while yes:
+    for a in event.get():
+        if a.type == QUIT:
+            yes = False
+
 quit()
